0c295f9eb2d02aee05facebd98e2266f638cf3b7
leblancg <37914048+leblancg@users.noreply.github.com>
2018-10-14 08:50:58 -0400

    Add files via upload

--- search.php
+++ search.php
@@ -21,7 +21,6 @@ $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-echo $mysqli->host_info . "<br>";
 
 // Print all rows of the database
 // $sql = "SELECT ID, ticker, growth, financial_returns, multiple, integrated_factor FROM stocks";
@@ -38,14 +37,14 @@ echo $mysqli->host_info . "<br>";
 // }
 
 // Search the database for a Ticker
-echo "<h2> Find the stock info with matching ticker </h2>";
-$sql = "SELECT ID, ticker, growth, financial_returns, multiple, integrated_factor FROM stocks WHERE ticker LIKE '%".$searchterm."%'";
+echo "<h2> Find the stock info with matching ticker: ".$searchterm." </h2>";
+$sql = "SELECT date, g, f, m, i, p, ticker FROM stocks2 WHERE ticker LIKE '%".$searchterm."%'";
 $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
 
 	while($row = $result->fetch_assoc()) {
-		echo "ID: " . $row["ID"]. " Ticker: " . $row["ticker"]. " Growth: " . $row["growth"]. " Financial Returns: " . $row["financial_returns"]. " Multiple: " . $row["multiple"]. " Integrated factor: " . $row["integrated_factor"]."<br>";
+		echo "ID: " . $row["date"]. " Ticker: " . $row["ticker"]. " Closing Price: " . $row["p"]." Growth: " . $row["g"]. " Financial Returns: " . $row["f"]. " Multiple: " . $row["m"]. " Integrated factor: " . $row["i"]."<br>";
 	}
 }
 else {
